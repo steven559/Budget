67598fddc48d82f168a63a00567a68c7855830d6
Stevens Dorchies <Trocme11>
2018-11-23 16:59:24 +0100

    init

--- evaluation.js
+++ evaluation.js
@@ -21,16 +21,16 @@ document.getElementById("td").innerHTML=depense.loyer;
 var but=document.getElementById("valider");
 
 var t=document.getElementById("1").innerHTML = depense.loyer;
-
+var val=document.getElementById("value");
 
 
 document.getElementById("2").innerHTML=depense["eau ,electricité"];
 document.getElementById("3").innerHTML=depense.telephone;
 document.getElementById("4").innerHTML=depense["assurance habitation"];
 document.getElementById("5").innerHTML=depense["assurance vehicules"];
-
+var ty=document.getElementById("t1");
 but.addEventListener("click",function () {
-
-
+    ty.innerHTML=val.value;
+console.log(val);
 });
 
